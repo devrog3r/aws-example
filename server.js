ffe08562ef2dd7b1c7d932bff1ff8a7ce6d32a58
devroger <ing.adrianrgonzalez@gmail.com>
2022-11-08 20:00:07 -0600

    Express V2

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 const express = require('express');
 
 const app = express();
-app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res) => res.end('Express con HTML V2!!!'))
+app.get('/', (req, res) => res.end('Hello world!!!'))
 
-//STARTING THE SERVER
-app.listen(app.get('port'), (error) => {
-    if (error) {
-        console.log('There Was an Error', $(error)); 
-    } else {
-        console.log('Server runing on Port', app.get('port'));
-    }
-    
-});
-
-
-// app.listen(3000);
-// console.log('Server on port 3000')
+app.listen(3000);
+console.log('Server on port 3000')
