8a5a4be0465441af5b2283398ff17e283cd76fed
devroger <ing.adrianrgonzalez@gmail.com>
2022-11-08 20:06:53 -0600

    Express V3

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 
 const app = express();
-
+app.set('port', process.env.PORT || 3000);
 app.get('/', (req, res) => res.end('Hello world!!!'))
 
-app.listen(3000);
-console.log('Server on port 3000')
+//STARTING THE SERVER
+app.listen(app.get('port'), (error) => {
+    if (error) {
+        console.log('There Was an Error', $(error)); 
+    } else {
+        console.log('Server runing on Port', app.get('port'));
+    }
+    
+});
+
+// app.listen(3000);
+// console.log('Server on port 3000')
